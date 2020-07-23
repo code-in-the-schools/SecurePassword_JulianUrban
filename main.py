151d62dc21d69dcb40eea176ea728a98da9105f5
thejduman <thejduman@yahoo.com>
2020-07-23 15:33:52 +0000

    More fixing

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ while validPassword == False: #code loops while there is an invalid password
   password = str(input("Enter a password: ")) #the user enters a password
   if len(password) < 8 or len(password) > 16: #is the password between 8 and 16 characters?
     print("Invalid password!")
-  elif password.islower() == True: #is the password in all lowercase? It needs to have uppercase and lowercase
+  elif password.islower() == True and password.isupper() == False: #is the password in all lowercase? It needs to have uppercase and lowercase
     print("Invalid password!")
   elif password.isalpha() == True: #is the password only letters? It needs to have numbers and/or special characters
     print("Invalid password!")
